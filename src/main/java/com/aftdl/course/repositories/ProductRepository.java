3f285b5368150394b5ed83f2661e0bb1693ac148
André Filipe Teixeira de Lima <andrefilipedelima@hotmail.com>
2025-03-20 16:49:41 -0300

    Product

---
+++ src/main/java/com/aftdl/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.aftdl.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aftdl.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
