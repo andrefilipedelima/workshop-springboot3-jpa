c1632dc5c08d131514475ddc999fe75c087ee709
André Filipe Teixeira de Lima <andrefilipedelima@hotmail.com>
2025-03-20 11:42:29 -0300

    Service layer, component registration

---
+++ src/main/java/com/aftdl/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.aftdl.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aftdl.course.entities.User;
+import com.aftdl.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
