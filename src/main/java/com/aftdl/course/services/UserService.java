965a05bf606c89f36a130c0b741dc4a54e643f60
André Filipe Teixeira de Lima <andrefilipedelima@hotmail.com>
2025-03-21 12:31:59 -0300

    Exception handling update

--- src/main/java/com/aftdl/course/services/UserService.java
+++ src/main/java/com/aftdl/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.aftdl.course.repositories.UserRepository;
 import com.aftdl.course.services.exceptions.DatabaseException;
 import com.aftdl.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -46,9 +48,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User user) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, user);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, user);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
